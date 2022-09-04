994ec10a8106661302079819609bf466f6a9a042
AntoshaLev <90862359+AntoshaLev@users.noreply.github.com>
2022-09-04 14:46:30 +0300

    two

--- authapp/views.py
+++ authapp/views.py
@@ -63,3 +63,13 @@ class ProfileEditorView(UserPassesTestMixin, UpdateView):
 
     def get_success_url(self):
         return reverse_lazy("authapp:profile_edit", args=[self.request.user.pk])
+
+    class ProfileEditorView(UserPassesTestMixin, UpdateView):
+        model = get_user_model()
+        form_class = forms.CustomUserChangeForm
+
+        def test_func(self):
+            return True if self.request.user.pk == self.kwargs.get("pk") else False
+
+        def get_success_url(self):
+            return reverse_lazy("authapp:profile_edit", args=[self.request.user.pk])
